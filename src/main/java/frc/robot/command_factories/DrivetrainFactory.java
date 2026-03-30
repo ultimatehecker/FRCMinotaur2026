158a15a1c37f29c29bedec39c20cb8fc7b1b005b
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-03-30 09:29:51 -0400

    set steer and drive requests

--- src/main/java/frc/robot/command_factories/DrivetrainFactory.java
+++ src/main/java/frc/robot/command_factories/DrivetrainFactory.java
@@ -8,6 +8,8 @@ import java.util.function.DoubleSupplier;
 import org.dyn4j.geometry.Rotation;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -31,61 +33,20 @@ public class DrivetrainFactory {
                     .withVelocityX(speeds.vxMetersPerSecond)
                     .withVelocityY(speeds.vyMetersPerSecond)
                     .withRotationalRate(speeds.omegaRadiansPerSecond)
+                    .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+                    .withSteerRequestType(SteerRequestType.MotionMagicExpo)
                 );
             } else {
                 drivetrain.setControl(new SwerveRequest.RobotCentric()
                     .withVelocityX(speeds.vxMetersPerSecond)
                     .withVelocityY(speeds.vyMetersPerSecond)
                     .withRotationalRate(speeds.omegaRadiansPerSecond)
+                    .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+                    .withSteerRequestType(SteerRequestType.MotionMagicExpo)
                 );
             }
         }, drivetrain);
     }
-
-    /*
-    
-    public static Command aimAtHub(Drivetrain drivetrain, RobotState robotState, DoubleSupplier throttleSupplier, DoubleSupplier strafeSupplier) {
-        SwerveRequest.FieldCentricFacingAngle aimRequest = new SwerveRequest.FieldCentricFacingAngle()
-            .withDeadband(DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond) * 0.05)  // 5% deadband
-            .withRotationalDeadband(DrivetrainConstants.kMaximumRotationalVelocity.in(RadiansPerSecond) * 0.05);
-        
-        return Commands.run(() -> {
-            double magnitudeX = MathUtil.applyDeadband(throttleSupplier.getAsDouble(),  ControllerConstants.kControllerDeadband);
-            double magnitudeY = MathUtil.applyDeadband(strafeSupplier.getAsDouble(), ControllerConstants.kControllerDeadband);
-            
-            double velocityX = robotState.isRedAlliance()  ? -magnitudeX * DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond) : magnitudeX * DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond);
-            double velocityY = robotState.isRedAlliance() ? -magnitudeY * DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond) : magnitudeY * DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond);
-            
-            Rotation2d targetAngle = Rotation2d.fromRadians(robotState.getAngleToTarget());
-            
-            // Apply skew compensation for translation
-            Rotation2d skewCompensationFactor = Rotation2d.fromRadians(robotState.getLatestMeasuredRobotRelativeChassisSpeeds().omegaRadiansPerSecond * -0.03);
-            drivetrain.setControl(aimRequest
-                .withVelocityX(velocityX)
-                .withVelocityY(velocityY)
-                .withTargetDirection(targetAngle.plus(skewCompensationFactor))
-            );
-        }, drivetrain).withName("Auto Align Hub UNLOCKED");
-    }
-   
-    public static Command aimAtHubLocked(Drivetrain drivetrain, RobotState robotState) {
-        SwerveRequest.FieldCentricFacingAngle aimRequest = new SwerveRequest.FieldCentricFacingAngle()
-            .withDeadband(DrivetrainConstants.kMaximumLinearVelocity.in(MetersPerSecond) * 0.05)
-            .withRotationalDeadband(DrivetrainConstants.kMaximumRotationalVelocity.in(RadiansPerSecond) * 0.05);
-        
-        return Commands.run(() -> {
-            Rotation2d targetAngle = Rotation2d.fromRadians(robotState.getAngleToTarget());
-            
-            // No translation - only rotation
-            drivetrain.setControl(aimRequest
-                .withVelocityX(0.0)
-                .withVelocityY(0.0)
-                .withTargetDirection(targetAngle)
-            );
-        }, drivetrain).withName("Auto Align Hub LOCKED");
-    }
-
-    */
 
     private static ChassisSpeeds calculateSpeedsBasedOnJoystickInputs(Drivetrain drivetrain, RobotState robotState, DoubleSupplier throttleSuppler, DoubleSupplier strafeSupplier, DoubleSupplier rotationSupplier) {
         if(DriverStation.getAlliance().isEmpty()) {
