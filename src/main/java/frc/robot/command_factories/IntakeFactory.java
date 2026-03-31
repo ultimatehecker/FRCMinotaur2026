5e0e121ec9656dc36f4cfe92df9e40bf6a946fdf
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-03-30 22:34:38 -0400

    changed some minor stuff

--- src/main/java/frc/robot/command_factories/IntakeFactory.java
+++ src/main/java/frc/robot/command_factories/IntakeFactory.java
@@ -10,18 +10,18 @@ import frc.robot.subsystems.intake.Intake.RollerGoal;
 public class IntakeFactory {
     public static Command prepareIntakeBlocking(RobotContainer robotContainer) {
         return Commands.parallel(
-            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.DEPLOY)).until(robotContainer.getIntake()::pivotAtGoal),
+            Commands.runOnce(() -> robotContainer.getIntake().setPivotSetpoint(PivotGoal.DEPLOY)).until(robotContainer.getIntake()::pivotAtGoal),
             Commands.startEnd(
-                () -> robotContainer.getIntake().setRollerGoal(RollerGoal.INTAKE),
-                () -> robotContainer.getIntake().setRollerGoal(RollerGoal.STOP)
+                () -> robotContainer.getIntake().setRollerOpenLoopVoltage(RollerGoal.INTAKE),
+                () -> robotContainer.getIntake().setRollerOpenLoopVoltage(RollerGoal.STOP)
             ).withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
         );
     }
 
     public static Command parkIntakeBlocking(RobotContainer robotContainer) {
         return Commands.parallel(
-            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.PARKED)),
-            Commands.runOnce(() -> robotContainer.getIntake().setRollerGoal(RollerGoal.STOP))
+            Commands.runOnce(() -> robotContainer.getIntake().setPivotSetpoint(PivotGoal.PARKED)),
+            Commands.runOnce(() -> robotContainer.getIntake().setRollerOpenLoopVoltage(RollerGoal.STOP))
         ).until(robotContainer.getIntake()::pivotAtGoal).withName("Retract Intake");
     }
 }
