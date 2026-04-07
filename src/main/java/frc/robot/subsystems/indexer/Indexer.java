455e5c3a627849611265fc0347bd8e6a669536a9
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-04-07 11:32:41 -0400

    indexer reprogrammed

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -16,105 +16,79 @@ import frc.minolib.advantagekit.LoggedTunableNumber;
 import frc.minolib.utilities.SubsystemDataProcessor;
 import frc.robot.constants.GlobalConstants;
 import frc.robot.constants.IndexerConstants;
-
+import frc.robot.subsystems.rollers.RollerSystem;
+import frc.robot.subsystems.rollers.RollerSystemIO;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 public class Indexer extends SubsystemBase {
-    private static final LoggedTunableNumber kP = new LoggedTunableNumber("Intake/kP");
-    private static final LoggedTunableNumber kD = new LoggedTunableNumber("Intake/kD");
-    private static final LoggedTunableNumber kS = new LoggedTunableNumber("Intake/kS");
-    private static final LoggedTunableNumber kV = new LoggedTunableNumber("Intake/kV");
-    private static final LoggedTunableNumber kA = new LoggedTunableNumber("Intake/kA");
+    private static final LoggedTunableNumber kP = new LoggedTunableNumber("Indexer/kP");
+    private static final LoggedTunableNumber kD = new LoggedTunableNumber("Indexer/kD");
+    private static final LoggedTunableNumber kS = new LoggedTunableNumber("Indexer/kS");
+    private static final LoggedTunableNumber kV = new LoggedTunableNumber("Indexer/kV");
+    private static final LoggedTunableNumber kA = new LoggedTunableNumber("Indexer/kA");
 
     @RequiredArgsConstructor
-    public enum Goal {
-        INTAKE(new LoggedTunableNumber("Indexer/IntakeVoltage", 12.0)),
+    public enum IndexerGoal {
+        STOP(new LoggedTunableNumber("Indexer/StopVoltage", 0.0)),
+        FEED(new LoggedTunableNumber("Indexer/IntakeVoltage", 12.0)),
         EXHAUST(new LoggedTunableNumber("Indexer/ExhaustVoltage", -6.0)),
-        IDLE(new LoggedTunableNumber("Indexer/IdleVoltage", 2.0)),
-        STOP(new LoggedTunableNumber("Indexer/StopVoltage", 0.0));
+        IDLE(new LoggedTunableNumber("Indexer/IdleVoltage", 1.5));
 
-        private final DoubleSupplier rollerVoltage;
+        private final DoubleSupplier voltage;
 
-        public double getRollerVoltage() {
-            return rollerVoltage.getAsDouble();
+        public double getVoltage() {
+            return voltage.getAsDouble();
         }
     }
 
     static {
         switch (GlobalConstants.getRobot()) {
             default -> {
-                kP.initDefault(IndexerConstants.kP);
-                kD.initDefault(IndexerConstants.kD);
-                kS.initDefault(IndexerConstants.kD);
-                kV.initDefault(IndexerConstants.kV);
-                kA.initDefault(IndexerConstants.kA);
+                kP.initDefault(0.0);
+                kD.initDefault(0.0);
+                kS.initDefault(0.0);
+                kV.initDefault(0.0);
+                kA.initDefault(0.0);
             }
             case SIMBOT -> {
-                kP.initDefault(IndexerConstants.simulatedKp);
-                kD.initDefault(IndexerConstants.simulatedKi);
-                kS.initDefault(IndexerConstants.simulatedKs);
-                kV.initDefault(IndexerConstants.simulatedKv);
-                kA.initDefault(IndexerConstants.simulatedKa);
+                kP.initDefault(0.0);
+                kD.initDefault(0.0);
+                kS.initDefault(0.0);
+                kV.initDefault(0.0);
+                kA.initDefault(0.0);
             }
         }
     }
 
-    private final IndexerIO io;
-    private final IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
+    private final RollerSystem leftRoller;
+    private final RollerSystem rightRoller;
+    @Getter @Setter @AutoLogOutput private IndexerGoal goal = IndexerGoal.STOP;
 
-    private final Debouncer indexerMotorConnectedDebouncer = new Debouncer(0.5, Debouncer.DebounceType.kFalling);
-    private final Alert indexerMotorDisconnectedAlert = new Alert("Intake pivot motor disconnected!", AlertType.kError);
-
-    @AutoLogOutput(key = "Indexer/BrakeModeEnabled")
-    private BooleanSupplier brakeModeEnabled = () -> false;
-
-    @Getter 
-    private Goal goal = Goal.STOP;
-    private double rollerVoltage = 0.0;
-
-    public Indexer(IndexerIO io) {
-        this.io = io;
-
-        SubsystemDataProcessor.createAndStartSubsystemDataProcessor(() -> {
-            synchronized (inputs) {
-                io.updateInputs(inputs);
-            }
-        },
-        io);
+    public Indexer(RollerSystemIO leftRollerIO, RollerSystemIO rightRollerIO) {
+        leftRoller = new RollerSystem("Indexer", "Indexer/LeftRoller", leftRollerIO);
+        rightRoller = new RollerSystem("Indexer", "Indexer/RightRoller", rightRollerIO);
     }
 
     @Override
     public void periodic() {
-        synchronized (inputs) {
-            Logger.processInputs("Indexer", inputs);
+        leftRoller.periodic();
+        rightRoller.periodic();
+
+        double rollerVoltage = 0.0;
+
+        switch(goal) {
+            case FEED, EXHAUST, IDLE -> {
+                rollerVoltage = goal.getVoltage();
+            }
+
+            case STOP -> {
+                rollerVoltage = 0.0;
+            }
         }
-    
-        switch (goal) {
-            case INTAKE, EXHAUST, IDLE -> rollerVoltage = goal.getRollerVoltage();
-            case STOP -> rollerVoltage = 0.0;
-        }
-        
-        io.setVoltage(rollerVoltage);
 
-        Logger.recordOutput("Indexer/AppliedVoltage", rollerVoltage);
-        Logger.recordOutput("Indexer/WantedVoltage", goal.getRollerVoltage());
-
-        LoggedTracer.record("IndexerPeriodicMS");
-    }
-
-    public void setGoal(Goal goal) {
-        if(goal == this.goal) return;
-        this.goal = goal;
-    }
-
-    public void setBrakeMode(BooleanSupplier enabled) {
-        if (this.brakeModeEnabled.getAsBoolean() == enabled.getAsBoolean()) return;
-        this.brakeModeEnabled = enabled;
-        io.setBrakeMode(brakeModeEnabled.getAsBoolean());
-    }
-
-    public double getVelocity() {
-        return inputs.velocity;
+        leftRoller.setVoltage(rollerVoltage);
+        rightRoller.setVoltage(rollerVoltage);
     }
 }
