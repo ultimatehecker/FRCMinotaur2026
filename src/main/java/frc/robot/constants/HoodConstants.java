9dbf1137bd7efdf9c3c8acd87f07d8ff19fe7943
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-04-29 23:18:28 -0400

    tracked gains to 0

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -26,20 +26,18 @@ public class HoodConstants {
     public static final Angle kHoodMaximumPosition = Degrees.of(47); 
     public static final Angle kHoodStartingPosition = Degrees.of(12);
 
-    public static final double kP = 450;
+    public static final double kP = 0.0;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
     public static final double kS = 0.0;
     public static final double kV = 0.0;
-    public static final double kG = 0.0;
     public static final double kA = 0.0;
 
     public static final boolean kMotorInverted = false;
     public static final double kMotorReduction = (25.0 / 1.0) * (36.0 / 48.0) * (190.0 / 10.0);
     public static final DCMotor kSimulatedGearbox = DCMotor.getKrakenX44Foc(1);
 
-    public static final Current kMotorStatorLimit = Amps.of(50);
-    public static final Current kMotorSupplyLimit = Amps.of(30);
+    public static final Current kMotorSupplyLimit = Amps.of(40);
 
     public static final Distance kHoodLength = Inches.of(7);
     public static final MomentOfInertia kHoodMOI = KilogramSquareMeters.of(0.1);
